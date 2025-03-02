8d8d4d03c8b5e5fcf3848a1c1280d77fb4125f1c
Zaprovic <jazaro2001@gmail.com>
2025-03-02 12:22:56 -0500

    sticky searchbar in products page

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { SearchProducts } from "./_components/search-products";
 
 const ProductsLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="fade-in mx-auto max-w-7xl">
-      <SearchProducts />
-      {children}
-    </div>
-  );
+  return <div className="fade-in mx-auto max-w-7xl">{children}</div>;
 };
 
 export default ProductsLayout;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,26 +4,26 @@ import { CategoryTabs } from "./_components/category-tabs";
 import { Suspense } from "react";
 // import { SearchProducts } from "./_components/search-products";
 import { LoaderIcon } from "lucide-react";
+import { SearchProducts } from "./_components/search-products";
 
 export default async function ProductsPage() {
   const categories = await db.select().from(categoriesTable);
 
   return (
-    <>
-      <>
-        <div className="container mx-auto my-17 max-w-7xl px-4 py-8">
-          {/* <SearchProducts /> */}
-          <Suspense
-            fallback={
-              <div className="flex flex-col items-center text-center">
-                <LoaderIcon className="h-10 w-10 animate-spin" />
-              </div>
-            }
-          >
-            <CategoryTabs categories={categories} />
-          </Suspense>
-        </div>
-      </>
-    </>
+    <div>
+      <SearchProducts />
+      <div className="container mx-auto my-17 max-w-7xl px-4 py-8">
+        {/* <SearchProducts /> */}
+        <Suspense
+          fallback={
+            <div className="flex flex-col items-center text-center">
+              <LoaderIcon className="h-10 w-10 animate-spin" />
+            </div>
+          }
+        >
+          <CategoryTabs categories={categories} />
+        </Suspense>
+      </div>
+    </div>
   );
 }
